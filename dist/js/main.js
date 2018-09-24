6ca464878789ccef5ff23e3ab1734ba2ac875889
prince kab <princekab601@gmail.com>
2018-09-24 23:45:01 +0100

    creating the markup, stylying it and add js eventListener

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,40 @@
+// get the modal element
+var modal = document.getElementById('simpleModal');
+//get the modal button
+
+var modalBtn = document.getElementById('modalBtn');
+
+//get the close Btn
+
+// var closeBtn = document.getElementById('closeBtn');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+//create a  eventListener for a click
+
+modalBtn.addEventListener('click', openModal);
+//listen for close click\
+closeBtn.addEventListener('click', closeModal);
+//for outside click
+window.addEventListener('click', clickOutSide);
+
+//create a function open modal
+
+function openModal(){
+    modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+//close the modal if click ouside
+
+function clickOutSide(e){
+    if(e.target == modal){
+        modal.style.display = 'none';
+    }
+}
+
+
+
+
+
 
